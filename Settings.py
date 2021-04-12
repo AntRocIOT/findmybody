29b1c4c620735204eac1a71f8c08048169fc56aa
Warrior <rokanas666@gmail.com>
2021-04-12 16:20:18 +0200

    Delete files

--- Settings.py
+++
@@ -1,5 +0,0 @@
-def init():
-    global zones,scroller,keyboard_player
-    zones = 0
-    scroller = None
-    keyboard_player = None
--- mapas/estrellas.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="estrellas" tilewidth="64" tileheight="64" tilecount="9" columns="3">
- <image source="estrellas.png" width="192" height="192"/>
-</tileset>
